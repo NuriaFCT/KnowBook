6c268a1152ce072111e8556b14099554f10dff29
nmunqui115Daw <91613004+nmunqui115Daw@users.noreply.github.com>
2022-06-09 14:14:52 +0200

    Añadido un boton en show book para regresar y funciona bien el dashboard

--- Proyecto/app/Http/Controllers/InicioController.php
+++ Proyecto/app/Http/Controllers/InicioController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-use App\Models\LIKE;
+use App\Models\POST;
+use stdClass;
 
 class InicioController extends Controller
 {
@@ -30,78 +31,57 @@ class InicioController extends Controller
     ->where('users.id', '=', $id)
     ->get();
 
-    $array_id_user_followers = array($id_user_follower);
+    $id= array($id_user_follower);
     
-    $posts = DB::table('posts')
-    ->join('users', 'posts.user_id', '=', 'users.id')
-    ->join('followers', 'followers.id_user_follower', '=', 'users.id')
-    ->get();
+    $array_ids=array();
 
+    foreach($id[0] as $id_user){
+        $array_ids[]=$id_user->id_user_follower;
+    }
     
-    $id_posts = DB::table('posts')
-    ->select('posts.id')
-    ->join('users', 'posts.user_id', '=', 'users.id')
-    ->join('followers', 'followers.id_user_follower', '=', 'users.id')
-    ->get();
-            
-    $isEmpty = empty($array_id_user_followers[0][0]);
+    //dd($id[0][1]->id_user_follower);
 
-    //dd($array_id_user_followers[0][0]);
-    //dd($isEmpty);
+    $posts= Post::all();
 
-    if(!$isEmpty){
+    $i=1;
 
-        foreach($array_id_user_followers as $id_user){
+    /*foreach($id[0] as $id_user){
 
-            //dd($id_user);
-            $posts->Where('followers.id_user_follower', $id_user);
+        print("IT ".$i. ":");
+        print_r($id_user->id_user_follower);
+        $posts=$posts->where('user_id',null, $id_user->id_user_follower);
+        $i++;
+        break;       
+    }*/
 
-            $id_posts->Where('followers.id_user_follower', '=' , json_encode($id_user));    
+    $post_filtrado= new stdClass;
+    $post_filtrado->items=array();
+    foreach($posts as $post){
+        if (in_array($post->user_id,$array_ids)){
+            $post_filtrado->items[]=$post;
         }
-    
-    }else{
-
-        $posts = DB::table('posts')
-        ->join('users', 'posts.user_id', '=', 'users.id')
-        ->join('followers', 'followers.id_user_follower', '=', 'users.id')
-        ->where('followers.id_user_follower', '=' , 0)
-        ->get();
+        
     }
 
     //dd($posts);
-
-    $array_id_posts = array($id_posts);    
-   
-
-    //LIKES DE LOS POST
-
-    $likes = LIKE::all()->pluck('post_id');
+    //dd($post_filtrado);
+    //dd($posts);
     
-    $likes->Where('post_id', 6);
-
-    //dd($likes);
-
-    //dd($array_id_posts); Tiene 8 valores, debe recorrer el foreach 8 veces
-
-   // foreach ($array_id_posts as  $id_post){
-        
-        //dd(json_encode($id_post));
-        
-        
-        //$likes ->where('likes.post_id', '=', json_encode($id_post));
-    //}
-    
+    //dd($id_user_follower);
+    /*$posts = DB::table('posts')
+    ->join('users', 'posts.user_id', '=', 'users.id')
+    ->join('followers', 'followers.id_user_follower', '=', 'users.id')
+    ->get();*/
 
     
 
-     //------------COMMENT. ------------Con esta consulta sacaremos el numero de comentarios de cada post
-     $comments = DB::table('comments')
-     ->select(DB::raw('count(comments.post_id) as contadorComentarios'))
-     ->where('comments.post_id', '=', 34)  //hay que cambiar el 34 igual que arriba
-     ->get();
+    /*foreach($id_user_follower as $id_user){
 
+        $posts= Post::all()->where('user_id', json_encode($id_user));
 
-    return view('dashboard', ["posts" => $posts, "likes"=>$likes, "comments"=>$comments]);
+    }*/
+
+    return view('dashboard', ["posts" => $post_filtrado->items]);
 
     }
 }  
--- Proyecto/resources/views/post/show.blade.php
+++ Proyecto/resources/views/post/show.blade.php
@@ -101,6 +101,11 @@
                                     @endif
 
                             @endif  
+                            <a href="{{ url('dashboard/') }}">
+                                <input type="button"
+                                    class='inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 '
+                                    style="background-color: #a05a2c; margin-left: 4px" value="Regresar">
+                            </a>
                         </div>
                     </div>
 
